805d3205ca7b5e5f15f3413074daab0d538b9501
Raphael Alhadeff <33775583+ralhadeff@users.noreply.github.com>
2019-01-03 14:09:58 -0800

    Update scaling.py

--- modules/scaling.py
+++ modules/scaling.py
@@ -1,3 +1,8 @@
+"""
+pandas DataFrame scaler that scales all numeric values and returns a scaled DataFrame
+returned DataFrame includes the untouched non-numeric values
+"""
+
 import pandas as pd
 import numpy as np
 import random
