55ef9e601008246772c4ed2559afdd6eda1cc2e7
Raphael Alhadeff <33775583+ralhadeff@users.noreply.github.com>
2019-01-14 13:03:18 -0800

    Update polynomial.py

--- modules/polynomial.py
+++ modules/polynomial.py
@@ -13,6 +13,22 @@ def add_polynimials(df,power=2,cross=2,skip_row=[]):
     power is same feature to the power, cross is cross multiplications between features
     if using cross, all cross multiplications will be added to the end
     """
+    # build skip list
+    mask = []
+    if (len(skip_row)>0):
+        if isinstance(skip_row[0],str):
+            for i in skip_row:
+                mask.append(df.columns.get_loc(i))
+        elif isinstance(skip_row[0],int):
+            mask = skip_row
+            # convert skip_row to names if using pandas DataFrame
+            names = []
+            if (type(df)==pd.DataFrame):
+                for i in skip_row:
+                    names.append(df.columns[i])
+                skip_row = names
+
+    
     # convert DataFrame to array
     pandas = False
     if (type(df)==pd.DataFrame):
@@ -21,35 +37,45 @@ def add_polynimials(df,power=2,cross=2,skip_row=[]):
         df = df.values
         pandas = True
     
+    # reverse the mask
+    rev = []
+    for i in range(df.shape[1]):
+        if (i not in mask):
+            rev.append(i)
+    mask = rev
+        
+    # add power polynomials
     new_df = df.copy()
     # skip 1 (is the original df), include power
     for i in range(2,power+1):
-        df_power = np.vectorize(lambda x: x**i)(df)
+        df_power = np.vectorize(lambda x: x**i)(df[:,mask])
         new_df = np.hstack((new_df,df_power))
         if (pandas):
-            new_columns = np.concatenate((new_columns,[name + "^" + str(i) for name in columns]))
+            new_columns = np.concatenate((new_columns,[name + "^" + str(i) for name in columns if name not in skip_row]))
     
+    # add cross multiplications
     #convert names to list
     new_columns = list(new_columns)    
     # cross will be done pair-wise
     for colA in range(df.shape[1]):
-        for colB in range(colA+1,df.shape[1]):
-            # skip 1 (original df), include cross
-            for i in range(2,cross+1):
-                # power of colA and colB
-                b = i-1
-                a = cross-b
-                df_cross = np.power(df[:,colA],a) * np.power(df[:,colB],b)
-                df_cross.shape = (new_df.shape[0],1)
-                new_df = np.hstack((new_df,df_cross))
-                if (pandas):
-                    string = columns[colA]
-                    if (a!=1):
-                        string+= '^' + str(a)
-                    string+='_x_' + columns[colB]
-                    if (b!=1):
-                        string+= '^' + str(b)
-                    new_columns.append(string)
+        if (colA in mask):
+            for colB in range(colA+1,df.shape[1]):
+                # skip 1 (original df), include cross
+                for i in range(2,cross+1):
+                    # power of colA and colB
+                    b = i-1
+                    a = cross-b
+                    df_cross = np.power(df[:,colA],a) * np.power(df[:,colB],b)
+                    df_cross.shape = (new_df.shape[0],1)
+                    new_df = np.hstack((new_df,df_cross))
+                    if (pandas):
+                        string = columns[colA]
+                        if (a!=1):
+                            string+= '^' + str(a)
+                        string+='_x_' + columns[colB]
+                        if (b!=1):
+                            string+= '^' + str(b)
+                        new_columns.append(string)
     
     if (pandas):
         new_df = pd.DataFrame(new_df,columns=new_columns)
