2d29e60b666632a984b1fb5ccd66f3e3a7a75d34
Raphael Alhadeff <33775583+ralhadeff@users.noreply.github.com>
2019-02-01 21:29:44 -0800

    Add files via upload

---
+++ LabelEncoder/label_encoder.py
@@ -0,0 +1,42 @@
+"""
+Label encoder
+"""
+
+import numpy as np
+import pandas as pd
+
+def encode(X,omit_one=True):
+    """
+    Label encoder - will transform a categorical column into a sparse matrix of labels
+        by default, skip the last value (all 0's)
+    User can provide a numpy array or a pandas DataFrame and output will match the type
+    """
+    # number of samples
+    m = len(X)
+    # unique labels
+    labels = np.unique(X)
+    # by default, omit one (which will be all 0's)
+    if (omit_one):
+        n = len(labels)-1
+        # print out for user's interest
+        print('omitting',labels[-1])
+    else:
+        n = len(labels)
+    # initialize results array
+    L = np.zeros((m,n),dtype=int)
+    # check whether output should be a numpy array or a pandas DataFrame
+    pandas = False
+    if (type(X) is pd.DataFrame):# or type(X) is pd.Series):
+        # transform X to 1d array for later use
+        X = X.values.ravel()
+        pandas = True
+    # populate sparse matrix
+    for i in range(n):
+        L[X==labels[i],i] = 1
+    if (pandas):
+        # convert back to DataFrame, using labels as column names
+        L = pd.DataFrame(L,columns=labels[:n])
+    return L
+
+if (__name__ == '__main__'):
+    print("This module is not intended to run by iself")
