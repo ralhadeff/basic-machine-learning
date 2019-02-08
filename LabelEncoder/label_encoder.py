46b7f8c6faecb5525de4e8b8685bc304408176b9
Raphael Alhadeff <33775583+ralhadeff@users.noreply.github.com>
2019-02-07 21:11:29 -0800

    Add files via upload

--- LabelEncoder/label_encoder.py
+++ LabelEncoder/label_encoder.py
@@ -5,7 +5,7 @@ Label encoder
 import numpy as np
 import pandas as pd
 
-def encode(X,omit_one=True):
+def encode(X,omit_one=None):
     """
     Label encoder - will transform a categorical column into a sparse matrix of labels
         by default, skip the last value (all 0's)
@@ -15,11 +15,23 @@ def encode(X,omit_one=True):
     m = len(X)
     # unique labels
     labels = np.unique(X)
+    skip = None
     # by default, omit one (which will be all 0's)
-    if (omit_one):
+    if (omit_one is None):
         n = len(labels)-1
         # print out for user's interest
         print('omitting',labels[-1])
+    elif (type(omit_one) is str):
+        # omit specific by name
+        n = len(labels)
+        # get index of the requested skip
+        skip = list(labels).index(omit_one)
+        print('omitting',labels[skip])
+    elif (type(omit_one) is int):
+        # omit specific by index
+        n = len(labels)
+        skip = omit_one
+        print('omitting',labels[skip])
     else:
         n = len(labels)
     # initialize results array
@@ -32,7 +44,15 @@ def encode(X,omit_one=True):
         pandas = True
     # populate sparse matrix
     for i in range(n):
-        L[X==labels[i],i] = 1
+        if (skip is None):
+            L[X==labels[i],i] = 1
+        else:
+            if (i != skip):
+                L[X==labels[i],i] = 1
+    # remove empty column if needed
+    if (skip is not None):
+        L = np.delete(L,skip,axis=1)
+        labels = np.delete(labels,skip)
     if (pandas):
         # convert back to DataFrame, using labels as column names
         L = pd.DataFrame(L,columns=labels[:n])
