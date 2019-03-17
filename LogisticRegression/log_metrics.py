fefcdeea95135cd5060879b10ba8bb82ed0eac95
Raphael Alhadeff <33775583+ralhadeff@users.noreply.github.com>
2019-03-16 18:07:50 -0700

    Update log_metrics.py

--- LogisticRegression/log_metrics.py
+++ LogisticRegression/log_metrics.py
@@ -1,22 +1,22 @@
-"""
+'''
 Logistic regression metrics tools
-"""
+'''
 
 import numpy as np
 
 def accuracy(real_y,predictions):
-    """
+    '''
     Total accuracy of classification (correct labels)
-    """
+    '''
     return (real_y==predictions).mean()
 
 def cross_entropy_loss(real_y,probabilities):
-    """
+    '''
     Cross entropy loss using : (yt log(yp) + (1 - yt) log(1 - yp))
         yt = y true
         yp = probability of y being 1
     Note that predictions here must be provided as probabilities
-    """
+    '''
     # check that predictions are correctly provided as probabilities (won't work for multiclass):
     if (np.unique(probabilities).shape == (2,)):
         raise ValueError("Input predictions should be probabilities, not labels")
@@ -25,12 +25,12 @@ def cross_entropy_loss(real_y,probabilities):
     return -(real_y*logyp + (1-real_y)*log1yp).mean()
 
 def roc(real_y,probabilities,points=None,area=False):
-    """
+    '''
     Receiver operating characteristic (ROC) curve
     plots the true-positive rate as a function of the false-positive rate
     Number of points to calculate the ROC curve can be provided (to increase precision)
     Area under the graph can be calculated if requested
-    """
+    '''
     # check that predictions are correctly provided as probabilities:
     if (np.unique(probabilities).shape == (2,)):
         raise ValueError("Input predictions should be probabilities, not labels")
@@ -76,10 +76,10 @@ def roc(real_y,probabilities,points=None,area=False):
         return fpr,tpr 
 
 def confusion_matrix(real_y,predictions,show=False):
-    """
+    '''
     Calculate and print the confusion matrix
     default return is matrix, print flag will instead print the output in a readable way
-    """
+    '''
     # determine the unique labels
     labels = len(np.unique(real_y))
     # construct confusion matrix
@@ -105,11 +105,11 @@ def confusion_matrix(real_y,predictions,show=False):
         return matrix
 
 def classification_report(real_y,predictions, decimal=2,show=False):
-    """
+    '''
     Calculate and return the classification report
     last row is the weighted average
     print flag will instead print the output in a readable way
-    """
+    '''
     labels = np.unique(real_y)
     matrix = confusion_matrix(real_y,predictions)
        
